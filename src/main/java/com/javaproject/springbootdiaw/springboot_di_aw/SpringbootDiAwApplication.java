9aa70c3a1b3b81a1071cf1e4a12017edbfe205f9
Vedant Karale <vedantkarale271@gmail.com>
2025-03-29 19:55:37 +0530

    springboot-di-aw base

---
+++ src/main/java/com/javaproject/springbootdiaw/springboot_di_aw/SpringbootDiAwApplication.java
@@ -0,0 +1,13 @@
+package com.javaproject.springbootdiaw.springboot_di_aw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDiAwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDiAwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaproject/springbootdiaw/springboot_di_aw/SpringbootDiAwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.springbootdiaw.springboot_di_aw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDiAwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
